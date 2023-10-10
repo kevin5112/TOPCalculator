962ec17508ad9b37b5d28e21d96bc0707d7aea72
Kevin Chen <kevin5112@yahoo.com>
2023-10-10 16:42:51 -0700

    add main functions of a calculator

--- app.js
+++ app.js
@@ -2,27 +2,27 @@ let firstNum = 0;
 let secondNum = 0;
 let operator = "+";
 
-function operate(firstNum, secondNum, operator) {
+function operate(firstNum, operator, secondNum) {
     let solution = 0;
+    console.log("operator", operator);
     switch(operator) {
         case "+":
-            solution = add(firstNum, secondNum);
+            setInputNumber(add(firstNum, secondNum));
             break;
         case "-":
-            solution = subtract(firstNum, secondNum);
+            setInputNumber(subtract(firstNum, secondNum));
             break;
         case "*":
-            solution = multiply(firstNum, secondNum);
+            setInputNumber(multiply(firstNum, secondNum));
             break;
         case "/":
-            solution = divide(firstNum, secondNum);
+            setInputNumber(divide(firstNum, secondNum));
             break;
         default:
             console.log("err");
     }
     return solution;
 }
-
 
 function add(x, y) {
     return x + y;
@@ -39,3 +39,67 @@ function multiply(x, y) {
 function divide(x, y) {
     return x / y;
 }
+
+let placeholderFlag = true;
+
+function getInputNumber() {
+    let input = document.querySelector(".input-number");
+    return input.textContent;
+}
+
+function setInputNumber(num) {
+    let input = document.querySelector(".input-number");
+    input.textContent = num;
+}
+
+function DisplayInput(number) {
+    let inputNum = 0;
+    let input = document.querySelector(".input-number");
+    if(placeholderFlag) {
+        input.textContent = "";
+        placeholderFlag = false;
+    }
+    input.textContent += number;
+    console.log(input);
+}
+
+let numbers = document.querySelectorAll(".number");
+numbers.forEach(num => {
+    num.addEventListener("click", (e) =>{ 
+        DisplayInput(e.target.textContent);
+    })
+});
+
+function Clear() {
+    let input = document.querySelector(".input-number");
+    input.textContent = "0";
+    placeholderFlag = true;
+}
+
+let clear = document.querySelector(".clearBtn");
+clear.addEventListener("click", () => Clear());
+
+
+
+// get the input before operation call
+let operation = document.querySelectorAll(".operation");
+operation.forEach(operation => {
+    operation.addEventListener("click", (e) => {
+        console.log(e.target.textContent);
+        console.log(getInputNumber());
+        // store operator and input number
+        firstNum = getInputNumber();
+        operator = e.target.textContent;
+        // then clear input
+        Clear();
+    })
+});
+
+let equalBtn = document.querySelector(".equalBtn");
+equalBtn.addEventListener("click", () => {
+    console.log(firstNum, operator, getInputNumber());
+    secondNum = getInputNumber();
+    operate(Number(firstNum), operator, Number(secondNum));
+    placeholderFlag = true;
+});
+
